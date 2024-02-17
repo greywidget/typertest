b9eabb2b59072e30fbaeaf1c392908ce5a19ae05
Craig Richards <craig@greywidget.co.uk>
2024-02-17 16:47:37 +0000

    Most basic example

---
+++ example_01.py
@@ -0,0 +1,13 @@
+"""First example from https://typer.tiangolo.com/tutorial/"""
+
+# The simplest Typer file...
+
+import typer
+
+
+def main():
+    print("Hello World")
+
+
+if __name__ == "__main__":
+    typer.run(main)
