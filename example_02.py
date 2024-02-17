978c10d1ea6e427b94f3de341ee5b565ea15e056
Craig Richards <craig@greywidget.co.uk>
2024-02-17 17:03:21 +0000

    Add Simple example with two arguments

---
+++ example_02.py
@@ -0,0 +1,13 @@
+"""Second example from https://typer.tiangolo.com/tutorial/"""
+
+# Let's add a CLI argument (or 2)
+
+import typer
+
+
+def main(name: str, age: int):
+    print(f"Hello {name}, what's it like to be {age}?")
+
+
+if __name__ == "__main__":
+    typer.run(main)
