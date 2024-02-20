35cae0994a0c1ab09bdc69190e3bcf798379f127
Craig Richards <craig@greywidget.co.uk>
2024-02-20 16:13:55 +0000

    CLI argument with  Custom Help Panels (via ich)

---
+++ example_20.py
@@ -0,0 +1,27 @@
+import typer
+from typing_extensions import Annotated
+
+"""Another example based on https://typer.tiangolo.com/tutorial/arguments/help/"""
+
+# If you have installed Rich, you can use `rich_help_panel` to show different
+# arguments in different panels
+
+
+def main(
+    name: Annotated[str, typer.Argument(help="Who to greet")],
+    lastname: Annotated[
+        str, typer.Argument(help="The last name", rich_help_panel="Secondary Arguments")
+    ] = "",
+    age: Annotated[
+        str,
+        typer.Argument(help="The user's age", rich_help_panel="Secondary Arguments"),
+    ] = "",
+):
+    """
+    Say hi to NAME very gently, like Dirk.
+    """
+    print(f"Hello {name}")
+
+
+if __name__ == "__main__":
+    typer.run(main)
