2e7897a8bb83e5fba0f06803d105cca6c0b61d4f
Craig Richards <craig@greywidget.co.uk>
2024-02-21 16:05:39 +0000

    CLI option with multiple Short Names together

--- INDEX.md
+++ INDEX.md
@@ -42,3 +42,4 @@ There examples are from the [excellent Typer documentation](https://typer.tiango
 - [CLI option with Custom Name and Short Name](example_34.py)
 - [CLI option with Short Name Only](example_35.py)
 - [CLI option with Custom Name and Default Name](example_36.py)
+- [CLI option with multiple Custom Names Together](example_37.py)
---
+++ example_37.py
@@ -0,0 +1,27 @@
+import typer
+from typing_extensions import Annotated
+
+"""Another example based on https://typer.tiangolo.com/tutorial/options/name/"""
+
+# CLI Option Short Names together
+# You can create multiple short names and use them together
+# N.B. You can only have a single non-boolean and it must go last so
+# it can receive the value so:
+# `python .\example_37.py -fn Widget` is fine
+# `python .\example_37.py -nf Widget` would throw an exception because
+# the `Str` parameter `--name` (`-n`) must go last to receive the value
+# `Widget`
+
+
+def main(
+    name: Annotated[str, typer.Option("--name", "-n")],
+    formal: Annotated[bool, typer.Option("--formal", "-f")] = False,
+):
+    if formal:
+        print(f"Good day Ms. {name}.")
+    else:
+        print(f"Hello {name}")
+
+
+if __name__ == "__main__":
+    typer.run(main)
